faa36fb76b7e3e81bb028b0611e82891118c88c1
nayan <nayankumarmpqa@gmail.com>
2021-02-22 08:37:05 +0530

    22nd feb core java commit2

--- src/udemy_Java_Concepts/BiNumberRunner.java
+++ src/udemy_Java_Concepts/BiNumberRunner.java
@@ -5,6 +5,8 @@ public class BiNumberRunner {
 	public static void main(String[] args) {
 		BiNumber bn = new BiNumber(3,5);
 
+		BiNumber bn2 = new BiNumber(2, 4);
+
 		System.out.println(bn.getN1());
 		System.out.println(bn.getN2());
 
@@ -14,5 +16,12 @@ public class BiNumberRunner {
 		
 		System.out.println(bn.getN1());
 		System.out.println(bn.getN2());
+
+		// I am using 2nd object for below code execution
+		System.out.println(bn2.doubleNum());
+
+		System.out.println(bn2.getN1());
+		System.out.println(bn2.getN2());
+
 	}
 }
