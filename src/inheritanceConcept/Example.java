02b76efd1f54a1968f34c6cd1e72403f2b9d069e
nayan <nayankumarmpqa@gmail.com>
2021-04-27 19:03:50 +0530

    inheritance practice 2

---
+++ src/inheritanceConcept/Example.java
@@ -0,0 +1,18 @@
+package inheritanceConcept;
+
+public class Example {
+
+	public static void main(String[] args) {
+
+		Student s = new Student();
+		s.setName("Nayan");
+		System.out.println(s.getName());
+		s.setAge(30);
+		System.out.println(s.getAge());
+
+		s.setRollNo(954895);
+		System.out.println(s.getRollNo());
+
+	}
+
+}
---
+++ src/inheritanceConcept/Student.java
@@ -0,0 +1,16 @@
+package inheritanceConcept;
+
+public class Student extends Person {
+
+	private int rollNo;
+
+	public int getRollNo() {
+		return rollNo;
+	}
+
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+
+
+}
